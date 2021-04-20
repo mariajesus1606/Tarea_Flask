9dc544e21c33b6ce70c3a8aa0cdf5c722879c975
mariajesus1606 <61273346+mariajesus1606@users.noreply.github.com>
2021-04-20 20:17:21 +0200

    Añadido pagina potencia

--- app.py
+++ app.py
@@ -8,4 +8,23 @@ def inicio():
     return render_template("principal.html")
 
 
+# Página potencia:
+@app.route('/potencia/<int:base>/<exp>',methods=["GET","POST"])
+def potencia(base,exp):
+    exp=int(exp)
+#Si el exponente es positivo, el resultado es la potencia.
+    if exp > 0:
+        resultado = base**exp
+#Si el exponente es 0, el resultado es 1.
+    elif exp==0:
+        resultado = 1
+#Si el exponente es negativo, el resultado es 1/potencia con el exponente positivo.
+    elif exp < 0:
+        exp = abs(exp)
+        resultado = float(1/(base**exp))
+    else:
+        abort(404)
+    return render_template("potencia.html",base=base,exponente=exp,resultado=resultado)
+
+
 app.run(debug=True)
