ce6d8f5ec0a1240faf8585503ac50459b999bde2
mariajesus1606 <61273346+mariajesus1606@users.noreply.github.com>
2021-04-20 20:04:37 +0200

    He modificado el fichero app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+from flask import Flask, render_template
+app = Flask (__name__)	
+
+#Pagina Principal
+@app.route('/')
+def inicio():
+    return render_template("principal.html")
+
+
+app.run(debug=True)
